278ea23a7255563f77da2e7f0f9909d0d85fdf9d
gsval <115543494+gsval@users.noreply.github.com>
2022-10-27 01:37:51 +0300

    исправлен баг с делением на 0

--- TraningRepository/TraningSolution/TraningProject_DivOperation,/div_operation.c
+++ TraningRepository/TraningSolution/TraningProject_DivOperation,/div_operation.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <locale.h>
+
 int main() {
-	int a, b;
-	float div;
-	scanf_s("%d %d", &a, &b);
-	div = a / b;
-	printf("%d", div);
-	system("pause");
-	return 0;
+
+    setlocale(LC_ALL, "russian");
+
+    int a, b;
+    float div;
+
+    printf("Введите первое число: ");
+    scanf_s("%d", &a);
+    printf("Введите второе число: ");
+    scanf_s("%d", &b);
+
+    if (b == 0) {
+        printf("На 0 делить нельзя!\n");
+    }
+    else {
+        if (a == 0) {
+            printf("Ответ: 0\n");
+        }
+        else {
+            div = a / b;
+            printf("Ответ: %.2f\n", div);
+        }
+    }
+    system("pause");
+    return 0;
 }
